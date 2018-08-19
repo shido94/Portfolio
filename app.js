a9027015060c6c02482e398fb5f1c2039164921b
Rupesh yadav <rupeshyadav94.ry@gmail.com>
2018-08-20 03:02:00 +0530

    node

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(function (req, res, next) {
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/dist/portfolio/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'portfolio', 'index.html'));
 });
 
 
